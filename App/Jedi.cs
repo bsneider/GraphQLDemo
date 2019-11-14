434d618d74a5654da7cbdd0d841ff4e9f11123d1
Brandon Sneider <54788551+bsneider@users.noreply.github.com>
2019-11-13 18:43:47 -0600

    more updates

--- App/Jedi.cs
+++ App/Jedi.cs
@@ -5,13 +5,5 @@ namespace App
         public int Id { get; set; }
         public string Name { get; set; }
         public string Side { get; set; }
-        public static IEnumerable<Jedi> GetJedis()
-        {
-            return new List<Jedi>() {
-            new Jedi () { Name = "Luke", Side = "Light" },
-            new Jedi () { Name = "Yoda", Side = "Light" },
-            new Jedi () { Name = "Darth Vader", Side = "Dark" }
-        };
-        }
     }
 }
--- App/Query.cs
+++ App/Query.cs
@@ -7,11 +7,27 @@ namespace App
         {
             return StarWarsDB.GetJedis();
         }
-
         [GraphQLMetadata("hello")]
         public string GetHello()
         {
             return "Hello Query class";
+        }
+        [GraphQLMetadata("jedi")]
+        public Jedi GetJedi(int id)
+        {
+            return StarWarsDB.GetJedis().SingleOrDefault(j => j.Id == id);
+        }
+
+    }
+    public class StarWarsDB
+    {
+        public static IEnumerable<Jedi> GetJedis()
+        {
+            return new List<Jedi>() {
+            new Jedi () { Name = "Luke", Side = "Light" },
+            new Jedi () { Name = "Yoda", Side = "Light" },
+            new Jedi () { Name = "Darth Vader", Side = "Dark" }
+        };
         }
     }
 }
