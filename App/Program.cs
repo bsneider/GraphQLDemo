c4447dabc3bdc0fabc99beac3ce212065eaeba07
Brandon Sneider <54788551+bsneider@users.noreply.github.com>
2019-11-13 18:27:57 -0600

    update program.cs
    add other random stuff

--- App/Program.cs
+++ App/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace App
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace App {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+            var schema = Schema.For (@"
+          type Query {
+              hello: String
+          }
+          ");
+
+            var root = new { Hello = "Hello World!" };
+            var json = schema.Execute (_ => {
+                _.Query = "{ hello }";
+                _.Root = root;
+            });
+
+            Console.WriteLine (json);
         }
     }
 }
