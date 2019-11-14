849c88ad7db667b1638b6f4206438773bda2db74
Brandon Sneider <54788551+bsneider@users.noreply.github.com>
2019-11-13 18:42:04 -0600

    added namespace

--- App/Jedi.cs
+++ App/Jedi.cs
@@ -1,6 +1,17 @@
-public class Jedi
+namespace App
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Side { get; set; }
+    public class Jedi
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Side { get; set; }
+        public static IEnumerable<Jedi> GetJedis()
+        {
+            return new List<Jedi>() {
+            new Jedi () { Name = "Luke", Side = "Light" },
+            new Jedi () { Name = "Yoda", Side = "Light" },
+            new Jedi () { Name = "Darth Vader", Side = "Dark" }
+        };
+        }
+    }
 }
--- App/Query.cs
+++ App/Query.cs
@@ -1,19 +1,17 @@
-public class Query {
-    [GraphQLMetadata ("jedis")]
-    public IEnumerable<Jedi> GetJedis () {
-        return StarWarsDB.GetJedis ();
-    }
+namespace App
+{
+    public class Query
+    {
+        [GraphQLMetadata("jedis")]
+        public IEnumerable<Jedi> GetJedis()
+        {
+            return StarWarsDB.GetJedis();
+        }
 
-    [GraphQLMetadata ("hello")]
-    public string GetHello () {
-        return "Hello Query class";
-    }
-
-    public static IEnumerable<Jedi> GetJedis () {
-        return new List<Jedi> () {
-            new Jedi () { Name = "Luke", Side = "Light" },
-            new Jedi () { Name = "Yoda", Side = "Light" },
-            new Jedi () { Name = "Darth Vader", Side = "Dark" }
-        };
+        [GraphQLMetadata("hello")]
+        public string GetHello()
+        {
+            return "Hello Query class";
+        }
     }
 }
